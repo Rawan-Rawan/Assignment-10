e81cdf9f3882c21c1d3ec49be3026c323428e530
Rawan <rawansalahat1@gmail.com>
2024-10-31 00:03:35 +0200

    first commit

---
+++ Assets/Script/Script.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      int y=5;
+     
+      for (int z=1; z<11; z++)
+      {
+        int result= Multiply(z,y);
+        int equ1 = Equation1(z);
+        int equ3 = Equation2(y);
+        Debug.Log(equ1+"x"+equ3+"="+result);
+      } 
+    }
+
+    int Multiply (int z, int y)
+     {
+       int Multiplication =z*y;
+       return Multiplication; 
+     }
+    int Equation1 (int z)
+     {
+       int equation1 = z;
+       return equation1; 
+     }
+      int Equation2 ( int y)
+     {
+       int equation2 = y;
+       return equation2; 
+     }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
